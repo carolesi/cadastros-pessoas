6d073a3a68419c50cfdbdf7645aab9588b47d48d
carolesi <carolspbarros@gmail.com>
2021-08-09 22:19:33 -0300

    Modifies salvar.php

--- salvar.php
+++ salvar.php
@@ -8,10 +8,22 @@ $cpf = $_POST['cpf'];
 $funcao = $_POST['funcao'];
 
 // inserir os dados preenchidos no banco de dados
-$sql = "INSERT INTO `tb_pessoas` (`pes_id`, `pes_nome`, `pes_cpf`, `pes_funcao`) VALUES (NULL, '$nome', '$cpf', '$funcao');";
+
+// se já tem um id, edita
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
+    $sql = "update tb_pessoas set pes_nome = '$nome', pes_cpf = '$cpf', pes_funcao = '$funcao' where pes_id = $id";
+
+// senão, insere
+} else {
+    $sql = "INSERT INTO `tb_pessoas` (`pes_id`, `pes_nome`, `pes_cpf`, `pes_funcao`) VALUES (NULL, '$nome', '$cpf', '$funcao');";
+}
 
 // executar o comando SQL
 mysqli_query($conexao, $sql);
+
+// fechando a conexão
+mysqli_close($conexao);
 
 ?>
 
